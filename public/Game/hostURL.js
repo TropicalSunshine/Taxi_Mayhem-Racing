e762998bf338553fa3e3887f061af05da33808d3
Jason C. Liu <32473162+TropicalSunshine@users.noreply.github.com>
2019-08-14 23:06:49 -0700

    Revert "up to date"
    This reverts commit acd9e584d7e153d27751805aad54923044382c56.

---
+++ public/Game/hostURL.js
@@ -0,0 +1,1 @@
+const hostURL = "https://boga.localtunnel.me";
---
+++ serverr.js
@@ -0,0 +1,18 @@
+const http = require("http");
+const app = require('./app');
+
+var socket = require("socket.io");
+
+const port = 4293;
+const server = http.createServer(app);
+
+const io = socket(server, {
+    path: "/controllerIO",
+    pingTimeout: 5000
+});
+
+require("./routes/Game").controllerIO(io);
+
+server.listen(port); 
+
+console.log(`listening on port ${port}......`);
