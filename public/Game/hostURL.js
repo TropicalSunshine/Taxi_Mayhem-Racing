acd9e584d7e153d27751805aad54923044382c56
Jason C. Liu <32473162+TropicalSunshine@users.noreply.github.com>
2019-08-14 23:03:16 -0700

    up to date

--- public/Game/hostURL.js
+++
@@ -1,1 +0,0 @@
-const hostURL = "https://boga.localtunnel.me";
--- server.js
+++ server.js
@@ -1,91 +1,18 @@
-const headers = {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
-    'Access-Control-Max-Age': 2592000, // 30 days
-    /** add other headers as per requirement */
-  };
+const http = require("http");
+const app = require('./app');
 
-//required libaries 
-var net = require('net');
-var http = require('http');
-var fs = require('fs');
-var express = require('express');
-var path = require('path');
-var c = require('crypto');
+var socket = require("socket.io");
 
-var devices = [];
-var deviceID = 0;
-var deviceData = '';
+const port = 4293;
+const server = http.createServer(app);
 
-//checks that ID to see if it already exists
-function checkID(IP){
-    for(let d = 0; d<devices.length; d++){
-        if(devices[d].IP == IP){
-            return false;
-        }
-    }
-    return true;
-}
+const io = socket(server, {
+    path: "/controllerIO",
+    pingTimeout: 5000
+});
 
-//finds device with that IP
-function findID(IP){
-    for(let d = 0;d < devices.length;d++){
-        if (devices[d].IP == IP){
-            return devices[d];
-        }
-    }
-}
+require("./routes/Game").controllerIO(io);
 
+server.listen(port); 
 
-//create net Server listening for TCP req
-//TCP Server
-var socketServer = net.createServer();
-
-socketServer.on("connection", function(socket){
-    reqIP = socket.remoteAddress.substring(7); 
-    if (checkID(reqIP)){
-        var device = {  
-            ID:deviceID, 
-            IP:socket.remoteAddress.substring(7),
-            DATA:'',
-            unique: ''
-        };
-        deviceID += 1;
-        devices.push(device);
-    }
-    var device = findID(reqIP);
-    socket.setEncoding('utf8');
-    socket.on('data', function(data){
-        var uniqueID = c.randomBytes(10).toString('hex');
-        device.DATA = data;
-        for(let i = 0; i<devices.length; i++){
-            if(devices[i].ID == device.ID){
-                devices[i].unique = uniqueID;
-            }
-        }
-        console.log(devices);
-    })
-})
-
-socketServer.listen(9001, function(){
-    console.log("TCP server listening on port 9001.....");
-})
-
-//create http
-//HTTP Server 
-var app = express();
-app.use(express.static(path.join(__dirname + '/public')));
-app.get("/data", function(req, res){
-    res.writeHeader(200, {"Content-Type": "text/plain"});
-    res.end(JSON.stringify(devices));
-})
-
-
-
-//--Google Cloud----
-
-var webserver = app.listen(process.env.PORT || '2498', function(){
-    console.log(`Web Server listening on ${webserver.address().port}`)
-})
-
-//app.listen(2498)
+console.log(`listening on port ${port}......`);
